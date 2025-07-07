52a8dcdbd071f9aeacfee1b88619f90f0f81bc8e
Shadmun Shahariar <109878144+ShadmanShahariar@users.noreply.github.com>
2025-07-07 17:39:08 +0600

    Create Invoce screen part 1

---
+++ InvoiceApp/Models/InvoiceDetails.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace InvoiceApp.Models
+{
+    public class InvoiceDetails
+    {
+        [Required]
+        public string Number { get; set; }
+        [Required]
+        public string Status { get; set; }
+        public DateOnly? IssueDate { get; set; }
+        public DateOnly? DueDate { get; set; }
+        [Required]
+        public string Service { get; set; }
+        [Range(1,999999,ErrorMessage ="Unit Price is not valid")]
+        public decimal UnitPrice { get; set; }
+        [Range(1,99)]
+        public int Quantity { get; set; }
+        [Required(ErrorMessage = "Client Name is required")]
+        public string ClientName { get; set; } = "";
+        [Required, EmailAddress]
+        public string Email { get; set; } = "";
+        [Phone]
+        public string Phone { get; set; } = "";
+        public string Address { get; set; } = "";
+    }
+}
